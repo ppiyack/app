e1623192a62664095a40614f1fa13efb38d723e9
JengYoung <young_pallete@naver.com>
2023-05-29 22:55:19 +0900

    feat(Card): add Card Styles

--- components/Item/Card.tsx
+++ components/Item/Card.tsx
@@ -1,16 +1,36 @@
+import {TComponentStyleObject} from '@/models/style/components';
+import {HeadSize, Skins, TextColors} from '@/utils/theme';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 interface ICard {
   title: string;
   description: string;
+  style: TComponentStyleObject;
 }
 
-export default function Card({title, description}: ICard) {
+export default function Card({style, title, description}: ICard) {
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{description}</Text>
+    <View style={[Styles.container, Skins.white, style]}>
+      <Text style={[Styles.title, HeadSize.h6, TextColors.dark]}>{title}</Text>
+      <Text style={[Styles.title, TextColors.dark]}>{description}</Text>
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 100,
+    borderRadius: 12,
+    padding: 20,
+  },
+
+  title: {
+    marginBottom: 12,
+  },
+
+  description: {
+    marginBottom: 12,
+  },
+});
