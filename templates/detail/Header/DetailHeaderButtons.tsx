a3a6be926853b448a395d159d50e2812f6f32539
JengYoung <young_pallete@naver.com>
2023-06-12 23:46:50 +0900

    feat(DetailHeaderButtons): create delete feed

--- templates/detail/Header/DetailHeaderButtons.tsx
+++ templates/detail/Header/DetailHeaderButtons.tsx
@@ -1,12 +1,32 @@
 import BaseButton from '@/components/Button/Button';
+import {useDetailContext} from '@/contexts/DetailContext';
+import {useAsyncStorage} from '@/hooks/useAsyncStorage';
+import {IData} from '@/models/detail';
+import {RootStackNavigationProp} from '@/screens/types';
 import {BorderRadius} from '@/utils/theme';
 import {TextColors} from '@/utils/theme';
 import {Flex} from '@/utils/theme';
 import {Skins} from '@/utils/theme';
+import {useNavigation} from '@react-navigation/native';
+
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 export default function DetailHeaderButtons() {
+  const navigation = useNavigation<RootStackNavigationProp>();
+
+  const {detail} = useDetailContext();
+
+  const [feeds, setFeeds] = useAsyncStorage<IData[], IData[]>({
+    key: 'feeds',
+    defaultValue: [],
+  });
+
+  const onDelete = () => {
+    setFeeds(feeds.filter(v => v.id !== detail?.data?.id));
+    navigation.navigate('Feed');
+  };
+
   return (
     <View style={[Style.container]}>
       <BaseButton
@@ -15,7 +35,9 @@ export default function DetailHeaderButtons() {
       </BaseButton>
       <BaseButton
         style={[Style.editButton, Flex.center, Skins.white, BorderRadius.soft]}>
-        <Text style={[TextColors.dark]}>삭제하기</Text>
+        <Text style={[TextColors.dark]} onPress={onDelete}>
+          삭제하기
+        </Text>
       </BaseButton>
     </View>
   );
